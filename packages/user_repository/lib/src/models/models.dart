111d72f2e614a3302415fc1da48df945cf3f8304
markolosic <markolosic1993@gmail.com>
2024-05-28 14:50:10 +0200

    Added new authentication

---
+++ packages/user_repository/lib/src/models/models.dart
@@ -0,0 +1,39 @@
+export 'user.dart';
+
+class MyUser {
+  String userId;
+  String email;
+  String name;
+  bool hasActiveCart;
+
+  MyUser({
+    required this.userId,
+    required this.email,
+    required this.name,
+    required this.hasActiveCart,
+  });
+
+  static final empty = MyUser(
+    userId: '',
+    email: '',
+    name: '',
+    hasActiveCart: false,
+  );
+
+  MyUserEntity toEntity() {
+    return MyUserEntity(
+      userId: userId,
+      email: email,
+      name: name,
+      hasActiveCart: hasActiveCart,
+    );
+  }
+
+  static MyUser fromEntity(MYUserEntity entity) {
+    return MyUser(
+        userId: entity.userId,
+        email: entity.email,
+        name: entity.name,
+        hasActiveCart: entity.hasActiveCart);
+  }
+}
